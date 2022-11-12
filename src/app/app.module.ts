5cbe61de0ecd35e3d91f88faefc6b5d515a16f3a
gawade07 <ketangawade111@gmail.com>
2022-11-12 14:07:52 +0530

    44

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HashLocationStrategy , LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +10,6 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
 import { NopageComponent } from './nopage/nopage.component';
-import { HashLocationStrategy , LocationStrategy } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,6 +23,17 @@ import { HashLocationStrategy , LocationStrategy } from '@angular/common';
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  exports
+
+  :[
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+    NopageComponent
   ],
   providers: [{provide:LocationStrategy, useClass:HashLocationStrategy}],
   bootstrap: [AppComponent]
